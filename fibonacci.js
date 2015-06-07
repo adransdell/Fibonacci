8fc82e8e98ae1f6856daf076e0f9854f5e9fd3c6
ashlynn <adransdell@gmail.com>
2015-06-06 23:28:27 -0700

    added 0 and 1

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+// Problem: Needs to know all Fibonacci numbers up until user inputs
+// Solution: Create Program that allows user to enter number and recieve Fibonacci Numbers
+
+
+//Create a number sequence starting with 0 and 1
+var beginSequence = [0 , 1];
+var answer = beginSequence[0] + beginSequence[1];
+
+// Add first numbers in sequence together
+
+ console.log(answer);
+//Use sum of numbers to add to previous number to get the next sum
+
+//Repeat
