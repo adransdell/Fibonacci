04c33ebb8453ea22178acac931b7bcfd2fa7d61a
ashlynn <adransdell@gmail.com>
2015-06-06 23:59:40 -0700

    added for loop

--- fibonacci.js
+++ fibonacci.js
@@ -2,13 +2,30 @@
 // Solution: Create Program that allows user to enter number and recieve Fibonacci Numbers
 
 
-//Create a number sequence starting with 0 and 1
+// Create a number sequence starting with 0 and 1
 var beginSequence = [0 , 1];
-var answer = beginSequence[0] + beginSequence[1];
 
 // Add first numbers in sequence together
+for(i = 0; i <= 10; i++){
 
- console.log(answer);
+  var answer = beginSequence[i] + beginSequence[i+1];
+  console.log(answer);
+
+}
+
+
 //Use sum of numbers to add to previous number to get the next sum
 
 //Repeat
+
+
+// //kyle things
+// var beginSequence = ['cat', 'bat','march','ash','capone',5,'brazil'];
+//
+// // Add first numbers in sequence together
+// for(i = 0; i <= 10; i++){
+//
+//   var answer = beginSequence[i] + beginSequence[i+1];
+//   console.log(answer);
+//
+// }
